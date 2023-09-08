2b731db749a12904b7fa439dc1dcc494acef5929
Prabharsha <89606147+Prabharsha@users.noreply.github.com>
2023-09-08 15:28:03 +0530

    project upload initated

---
+++ app.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const express = require('express');
+const axios = require('axios');
+const port = 9321;
+
+const app = express();
+
+const server = http.createServer(app);
+
+
+/*
+* --> For : NSB IB reCaptcha validation
+* --> Params : user generate token
+* --> Response : validation response
+*/
+app.post('/nsb-ib/reCaptcha/validate', (req, res) => {
+    const token = req.query.token;
+
+    console.log('Token -->' + token)
+
+    axios({
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        baseURL: 'https://www.google.com',
+        url: '/recaptcha/api/siteverify',
+        params: {
+            secret: process.env.NSB_IB_SECRET_KEY,
+            response: token
+        }
+    }).then((response) => {
+        res.send(response.data);
+        console.log('Validation response for humam --> ' + response.data.success)
+    }).catch((error) => {
+        res.send('Error validating reCAPTCHA');
+    });
+});
+
+app.listen(port, function (error) {
+    if (error) {
+        console.log("Server error --> " + error)
+    } else {
+        console.log("Server is listening on port --> " + port)
+    }
+})
